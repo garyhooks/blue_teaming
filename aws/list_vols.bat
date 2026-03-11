423297f76c8f9f9eec058967da8e7bd747b394ec
Gary Hooks <garyhooks@gmail.com>
2026-03-11 10:36:42 +0000

    Update list_vols.bat

--- aws/list_vols.bat
+++ aws/list_vols.bat
@@ -8,5 +8,4 @@ aws ec2 describe-volumes --region ap-southeast-1 --filters Name=status,Values=* 
 aws ec2 describe-volumes --region ap-southeast-2 --filters Name=status,Values=* --query "Volumes[*].[AvailabilityZone, Size, CreateTime, VolumeId, State, Tags[?Key=='Name']|[0].Value]" --output text >> vols.txt
 
 aws ec2 describe-volumes --region us-east-1 --filters Name=status,Values=* --query "Volumes[*].[AvailabilityZone, Size, CreateTime, VolumeId, State, Tags[?Key=='Name']|[0].Value]" --output text >> vols.txt
-
 ```
