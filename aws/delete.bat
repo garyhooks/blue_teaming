b787b50c8013c1c11ddc64a54b549985fa48e11e
Gary Hooks <garyhooks@gmail.com>
2026-03-11 10:35:33 +0000

    Create delete.bat

---
+++ aws/delete.bat
@@ -0,0 +1,7 @@
+```bat
+aws ec2 --region ap-southeast-2 delete-volume --volume-id vol-0659ad9655151xyzz
+aws ec2 --region us-east-1 delete-volume --volume-id vol-0659ad9655151xyzz
+aws ec2 --region eu-west-1 delete-volume --volume-id vol-0659ad9655151xyzz
+aws ec2 --region eu-west-2 delete-volume --volume-id vol-0659ad9655151xyzz
+aws ec2 --region ap-southeast-1 delete-volume --volume-id vol-0659ad9655151xyzz
+```
